4a024ae658840e704add22739d406a74ee57cc93
Mackdesign <marcio@design2u.com.br>
2021-07-29 20:03:21 -0300

    Estilizando modal & criando botões de Tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
 import Modal from 'react-modal';
 
 interface NewTransactionModalProps {
@@ -8,14 +11,57 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   return (
-    <Container>
-      <Modal 
+   
+      <Modal
         isOpen={isOpen} 
         onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
       >
-        
-        <h2>Cadastrar Transação</h2>
+
+       <button 
+          type="button" 
+          onClick={onRequestClose}
+          className="react-modal-close"
+        >
+          <img src={closeImg} alt="Fechar Modal"/>  
+       </button> 
+       <Container>
+         <h2>Cadastrar Transação</h2>
+
+         <input 
+           placeholder="Título"
+          />
+
+         <input 
+           type="number"
+           placeholder="Valor"
+          />
+
+          <TransactionTypeContainer>
+            <button
+              type="button"
+            >
+              <img src={incomeImg} alt="Entrada" />
+              <span>Entrada</span>
+            </button>
+
+            <button
+              type="button"
+            >
+              <img src={outcomeImg} alt="" />
+              <span>Saída</span>
+            </button>
+          </TransactionTypeContainer>
+
+          <input 
+           placeholder="Categoria"
+          />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+       </Container>
       </Modal>
-    </Container>
-  )
+  );
 }
