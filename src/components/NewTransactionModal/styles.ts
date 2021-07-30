6f8261f5cc1245cb6b22630e865bfc7936c220dc
Mackdesign <marcio@design2u.com.br>
2021-07-29 22:50:22 -0300

    Inserindo transação na API

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -50,7 +50,7 @@ export const Container = styled.form`
 `;
 
 
-export const TransactionTypeContainer = styled.form`
+export const TransactionTypeContainer = styled.div`
   margin: 1rem 0;
   display: grid;
   grid-template-columns: 1fr 1fr;
