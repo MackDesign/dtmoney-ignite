abb1d17b3085db35fecd638d8a64e79dfda911c1
Mackdesign <marcio@design2u.com.br>
2021-07-30 03:18:21 -0300

    Criando hook

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState, createContext, ReactNode } from "react";
-import { api } from "./services/api";
+import { useEffect, useState, createContext, ReactNode, useContext } from "react";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number;
@@ -21,7 +21,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
   );
 
@@ -52,6 +52,13 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     </TransactionsContext.Provider>
   )
 } 
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
+}
+
 
 
 
